98e8bc7f8cbec1d468f8b2fecb74c4a758911de5
UĞUR BAĞCI <ugur_bagci_@hotmail.com>
2019-06-21 15:24:45 +0300

    Add project files.

---
+++ MVCHelpers/Controllers/AntiForgeyTokenController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCHelpers.Controllers
+{
+    public class AntiForgeyTokenController : Controller
+    {
+        // GET: AntiForgeyToken
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCHelpers/Controllers/BaseController.cs
@@ -0,0 +1,22 @@
+﻿using MVCHelpers.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCHelpers.Controllers
+{
+    public class BaseController : Controller
+    {
+        NorthwindEntities db;
+        public BaseController()
+        {
+            db = new NorthwindEntities();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+        }
+    }
+}
